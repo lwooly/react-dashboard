80a712d551374722ede6758e5fb0f36239212905
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2023-10-16 09:57:20 +0100

    api requests https

--- src/utilityFns/buildWeatherURL.js
+++ src/utilityFns/buildWeatherURL.js
@@ -4,9 +4,9 @@ export const buildWeatherURL = ( location, APIkey = WEATHERAPIKEY,) => {
 
     // console.log(location)
     if (location.latitude && location.longitude) {
-        return `http://api.weatherapi.com/v1/current.json?key=${APIkey}&q=${location.latitude},${location.longitude}&aqi=no`
+        return `https://api.weatherapi.com/v1/current.json?key=${APIkey}&q=${location.latitude},${location.longitude}&aqi=no`
     } else {
-        return `http://api.weatherapi.com/v1/current.json?key=${APIkey}&q=${location}&aqi=no`
+        return `https://api.weatherapi.com/v1/current.json?key=${APIkey}&q=${location}&aqi=no`
     }
     
     
